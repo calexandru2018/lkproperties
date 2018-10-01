153d467bd5de47da0cbad45ea44d7227291494b6
Sashkaz <31934100+Sashkaz@users.noreply.github.com>
2018-10-01 21:23:00 +0200

    fixed some code for upload

--- admin/assets/js/upload.js
+++ admin/assets/js/upload.js
@@ -5,12 +5,10 @@ function uploadPhotos(url, queryCollection){
     this.upload = function(){
         queryCollection.forEach(function(el){
             el.addEventListener('submit', e => {
-                e.preventDefault();
                 loadingGifEl = el.nextElementSibling;
                 button = el.closest('form').querySelector('input[type=submit]');
-                console.log(loadingGifEl);
-                console.log(button);
                 
+                e.preventDefault();
                 if (loadingGifEl.classList.contains('hidden')) {
                     loadingGifEl.classList.remove('hidden');
                     button.classList.add('hidden');
@@ -23,7 +21,7 @@ function uploadPhotos(url, queryCollection){
                     var contentID = el.closest('tr').getAttribute('data-content-id');
                 }
                 let formData = new FormData();
-                
+
                 for (let i = 0; i < files.length; i++) {
                     let file = files[i];
                     formData.append('image_field[]', file, contentID + '___' + file.name);
@@ -36,8 +34,8 @@ function uploadPhotos(url, queryCollection){
                 .then(response => response.text())
                 .then(data => {
                     if (button.classList.contains('hidden')) {
-                        button.classList.remove('hidden');
                         loadingGifEl.classList.add('hidden');
+                        button.classList.remove('hidden');
                     }
                     if(Number.isInteger(parseInt(data)) != true)
                         alert('Erro ao inserir na base de dados');
@@ -46,8 +44,8 @@ function uploadPhotos(url, queryCollection){
                 })
                 .catch(function(error){
                     if (button.classList.contains('hidden')) {
-                        button.classList.remove('hidden');
                         loadingGifEl.classList.add('hidden');
+                        button.classList.remove('hidden');
                     }
                     alert('Erro em fazer upload');
                     console.log(error);
