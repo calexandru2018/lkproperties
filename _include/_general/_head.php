418a995ddb0aa0de438e30732272396770bc9bf0
calexandru2018 <calexandru2018@mail.com>
2018-10-10 22:57:15 +0200

    improved webpage loading time

--- _include/_general/_head.php
+++ _include/_general/_head.php
@@ -6,21 +6,24 @@
     <link rel="alternate" hreflang="pt" href="https://lk-properties.pt/pt" />
     <link rel="alternate" href="http://example.com/en" hreflang="x-default" />
 
+    <link rel="preload" href="<?php echo $GLOBALS['absPath']; ?>assets/css/bootstrap-reboot.css" as="style">
+    <link rel="preload" href="<?php echo $GLOBALS['absPath']; ?>assets/css/bootstrap.css" as="style">
+    <link rel="preload" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" as="style" crossorigin>
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900" as="style">
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" as="style" crossorigin>
+    <link rel="preload" href="https://code.jquery.com/jquery-3.3.1.slim.min.js" as="script" crossorigin>
+    <link rel="prefetch" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css" as="style">
+    <link rel="prefetch" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"" as="style">
+
     <link rel="stylesheet" href="<?php echo $GLOBALS['absPath']; ?>assets/css/bootstrap-reboot.css">
     <link rel="stylesheet" href="<?php echo $GLOBALS['absPath']; ?>assets/css/bootstrap.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css" />
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css"/>
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/solid.css" integrity="sha384-wnAC7ln+XN0UKdcPvJvtqIH3jOjs9pnKnq9qX68ImXvOGz2JuFoEiCjT8jyZQX2z" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/brands.css" integrity="sha384-nT8r1Kzllf71iZl81CdFzObMsaLOhqBU1JD2+XoAALbdtWaXDOlWOZTR4v1ktjPE" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' type='text/css'>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <title>LK-Properties</title>
 </head>
