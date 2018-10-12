6795115f10bfa44c8bd02b81989dbc9ceac90f58
calexandru2018 <calexandru2018@mail.com>
2018-10-12 15:57:12 +0200

    fixed styling

--- _include/_general/_cookie-prompt.php
+++ _include/_general/_cookie-prompt.php
@@ -3,6 +3,7 @@
         position: fixed !important;
         bottom: 0 !important; 
         z-index: 9999;
+        box-shadow: 0px 30px 45px 10px;
     }
 </style>
 <div class="container-fluid py-2 bg-light cookie-notice">
@@ -11,7 +12,7 @@
             <p class="font-weight-bold mt-3 text-center"><?php echo $lang['cookie']['cookieModalInformation']; ?></p>
         </div>
         <div class="col-12 col-md-4 text-center text-md-left">
-            <button type="button" class="btn btn-info text-uppercase font-weight-bold mt-1" id="cookieAgreement"><?php echo $lang['cookie']['cookieModalButton']; ?></button>
+            <button type="button" class="btn btn-info text-uppercase font-weight-bold mt-2" id="cookieAgreement"><?php echo $lang['cookie']['cookieModalButton']; ?></button>
         </div>
     </div>
 </div>
